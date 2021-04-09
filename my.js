fa6741411bb96203432aa974d6df58a5b3d38e22
canhthaomodule1c0221h2 <79823013+canhthaomodule1c0221h2@users.noreply.github.com>
2021-04-09 16:48:11 +0700

    Add files via upload

---
+++ my.js
@@ -0,0 +1,7 @@
+let age = +prompt("nhập vào số tuổi");
+if (age > 0 && age <120) {
+    document.write("đây là số tuổi của người");
+}
+else {
+    document.write("đây không phải số tuổi của người")
+}
